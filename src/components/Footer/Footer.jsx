5b9440acfb94dccbfa9fc5b2ba70634a8414a9b2
Guddukumar2003 <guddu200345@gmail.com>
2025-05-10 09:24:36 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+import "./style.css"
+import { Col, Container, Row } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+          <Row className="footer-row">
+            <Col md={3} sm={5} className='box'>
+              <div className="logo">
+                  <ion-icon name="bag"></ion-icon>
+                  <h1>Sphopefast</h1>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>About Us</h2>
+              <ul>
+                <li>Careers</li>
+                <li>Our Stores</li>
+                <li>Our Cares</li>
+                <li>Terms & Conditions</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Customer Care</h2>
+              <ul>
+                <li>Help Center </li>
+                <li>How to Buy </li>
+                <li>Track Your Order </li>
+                <li>Corporate & Bulk Purchasing </li>
+                <li>Returns & Refunds </li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Contact Us</h2>
+              <ul>
+                <li>Dheeraj Nagar, Yamuna Inclave, Block(B), Faridabad,Haryana </li>
+                <li>Email: guddu200345@gmail.com</li>
+                <li>Phone: +91 8587992103</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
